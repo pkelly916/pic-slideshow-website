e138459aa0e0b93970c7db515ae966b9ff6240e4
Paul Kelly <pjkelly916@gmail.com>
2025-02-07 16:05:18 -0800

    added mobile functionality

--- script/getrand.sh
+++ script/getrand.sh
@@ -4,5 +4,10 @@
 echo "Content-type: text/html"
 echo ""
 
-ls /usr/share/nginx/www/pics | shuf -n 1 
+# mobile condition
+if [[ -n "$QUERY_STRING" ]]; then
+    echo "mobile/$(ls /usr/share/nginx/www/mobile | shuf -n 1)"
+else # desktop condition
+    echo "pics/$(ls /usr/share/nginx/www/pics | shuf -n 1)"
+fi
 
